f204d021e8ee0f289b08d2b0be8e0c7c59ca4eb9
Francesco Giannicola <francescogiannicola1@gmail.com>
2026-03-03 02:50:48 +0100

    Initial Commit

---
+++ Bite/BiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiteApp.swift
+//  Bite
+//
+//  Created by Francesco Giannicola on 03/03/26.
+//
+
+import SwiftUI
+
+@main
+struct BiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BiteTests/BiteTests.swift
@@ -0,0 +1,17 @@
+//
+//  BiteTests.swift
+//  BiteTests
+//
+//  Created by Francesco Giannicola on 03/03/26.
+//
+
+import Testing
+@testable import Bite
+
+struct BiteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
